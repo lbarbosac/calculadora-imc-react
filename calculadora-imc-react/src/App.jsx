68ca826ce92723172d93d2584b0ad366dbd0a90f
Cassiano Ramos <cassianolori5@gmail.com>
2026-04-24 12:07:49 -0300

    .

---
+++ calculadora-imc-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Simple React Project!</h1>
+      <p>Este é um projeto simples para verificar se o React está funcionando corretamente.</p>
+   
+   
+   
+   
+    </div>
+  );
+};
+
+export default App;
